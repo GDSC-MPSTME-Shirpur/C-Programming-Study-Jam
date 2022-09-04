24330fd2fc0554645e53ec23f261583c884ba2e8
Aman Singh Bhogal <45331015+AmanSinghBhogal@users.noreply.github.com>
2022-09-04 14:23:11 +0530

    Updated Introduction Directory.

--- Introduction/DataTypes.c
+++ Introduction/DataTypes.c
@@ -20,6 +20,11 @@
 int main()
 {
     // we use printf() statement to display output to the console.
-    printf("Hello World.");
+    printf("Data Types in C.");
+    int a = 51;                     // Normal Integer Number
+    char ch = 'a';                  // Character
+    float temperature = 32.4;       // Floating point Number
+    double pi = 3.14777777;         // Large Floating point Number
+    long b = 1000001;               // Large Integer Number
     return 0;
 }
---
+++ Introduction/Input_Output.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    // Outputs:
+    printf("Hello World.\n");
+    
+    // Printing Values of Variables:
+    int a = 10;
+    printf("Value of A is: %d \n",a);
+
+    float b = 10.25;
+    printf("Value of B is: %f \n",b);
+
+    char ch = 'a';
+    printf("Value of ch is: %c \n",ch);
+
+}
