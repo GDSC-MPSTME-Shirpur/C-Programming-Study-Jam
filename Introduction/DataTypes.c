eee8adc79687a29d1e9ffbc5f2abbb89c3742c66
Aman Singh Bhogal <45331015+AmanSinghBhogal@users.noreply.github.com>
2022-09-04 13:56:50 +0530

    Added gitignore file

---
+++ Introduction/DataTypes.c
@@ -0,0 +1,25 @@
+/*
+
+# Comments in C:
+   - In programming, comments are hints that a programmer can add to make their code easier to read and understand. 
+*/
+
+// This is a Single Line Comment
+
+/*  
+    This
+    is
+    a
+    Multi-Line
+    Comment
+*/
+
+#include<stdio.h>   // This is an Header file.
+// stdio.h stands for Standard Input-Output Header File.
+
+int main()
+{
+    // we use printf() statement to display output to the console.
+    printf("Hello World.");
+    return 0;
+}
